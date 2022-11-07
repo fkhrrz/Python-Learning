5deeb00598f890b5a8be03e7425abfe8dd8e0026
Fakhrurrozi, M <muhammadfakhrurrozi71@gmail.com>
2022-11-07 18:01:02 +0700

    Variables basic

---
+++ 4-Variables.py
@@ -0,0 +1,11 @@
+greeting = "This is a sample of string which became a value of variable called greeting"
+print(greeting)
+
+rules = """
+There is a rule in making a variable
+1. Variable name must start with a letter or the underscore character.
+2. Variable name can't be started with number.
+3. Variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+4. Variable names are case-sensitive (age, Age and AGE are three different variables).
+"""
+print(rules)
