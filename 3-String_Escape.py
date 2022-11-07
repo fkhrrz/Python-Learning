57680d6b89924eafec91285e70fe2904c1769274
Fakhrurrozi, M <muhammadfakhrurrozi71@gmail.com>
2022-11-07 15:46:24 +0700

    Escaping string

---
+++ 3-String_Escape.py
@@ -0,0 +1,24 @@
+print("This is\na multiple line\nstring")
+
+print("This\tis\ta\ttab\tseparated\tstring\n")
+
+print("This is a \"quotation escape\"\n")
+
+print("""This is a "quotation escape" but without backslashing char""")
+
+print("""
+This is
+a multiple line sample
+without using new line escape
+""")
+
+print("""\
+This is \
+a sample of \
+single line string \
+which written \
+in several lines
+""")
+
+print("This is a sample of backslash escaping which allow us to use \\ in a string")
+print(r"or you can add a lowercase 'r' in front of the string to escape every \ on a string", "\n")
